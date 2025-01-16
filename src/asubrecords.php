46f0c5c1bae2abf2086603346246522cb1a41e02
Vihiga Tyonum <withtvpeter@gmail.com>
2025-01-16 21:55:52 +0100

    update pages

--- src/asubrecords.php
+++ src/asubrecords.php
@@ -1,10 +1,10 @@
 <?php session_start();
-    require 'bridge.php';
+require 'bridge.php';
 
 $session = $query->sSession("session", "sessionstatus");
 if ($_SESSION['role'] == "staff") {
-  $classes = $query->selectAndOrder(["class"], "tclasses", ["name"=>$_SESSION['_id_']], "class", "ASC");
-}elseif ($_SESSION['role'] == "admin") {
+  $classes = $query->selectAndOrder(["class"], "tclasses", ["name" => $_SESSION['_id_']], "class", "ASC");
+} elseif ($_SESSION['role'] == "admin") {
   $classes = $query->selectColumn("class", "class");
 }
 $gterm = $query->term();
@@ -12,8 +12,7 @@ $subs = $query->selectColumn("subject", "subject");
 
 if (isset($_POST['check']) && isset($_POST['session']) && isset($_POST['class']) && isset($_POST['subject'])) {
 
-	$subjectRecords = $query->annualSubject($_POST['session'], $_POST['class'], $_POST['subject']);
-	
+  $subjectRecords = $query->annualSubject($_POST['session'], $_POST['class'], $_POST['subject']);
 }
-    
+
 require 'view/admin/asubrecords.view.php';
--- src/promote.php
+++ src/promote.php
@@ -1,50 +1,66 @@
 <?php session_start();
 require 'bridge.php';
 
-$classes = $query->promoteClasses($_GET['class']); 
+$classes = $query->promoteClasses($_GET['class']);
 
-      if(isset($_GET['cid'])){ $admno =$_GET['cid']; }
-     if(isset($_GET['class'])) { $class =$_GET['class']; }
-     if(isset($_GET['session'])) { $session =$_GET['session']; }
-    
-     $newS = substr( $session, -4); $nn = $newS+1; $nsession = $newS.'/'.$nn;
+if (isset($_GET['cid'])) {
+  $admno = $_GET['cid'];
+}
+if (isset($_GET['class'])) {
+  $class = $_GET['class'];
+}
+if (isset($_GET['session'])) {
+  $session = $_GET['session'];
+}
 
-     $checkP = $query->lookUp("name", "students", ["admno"=>$admno, "session"=>$nsession]);
+$newS = substr($session, -4);
+$nn = $newS + 1;
+$nsession = $newS . '/' . $nn;
 
-$studentDetails = $query->selectRow(["name", "passport", "dob", "sex", "dater", "address", "phone", "email", "class"], "students", ["admno"=>$admno, "class"=>$class, "session"=>$session]);
+$checkP = $query->lookUp("name", "students", ["admno" => $admno, "session" => $nsession]);
+
+$studentDetails = $query->selectRow(["name", "passport", "dob", "sex", "dater", "address", "phone", "email", "class"], "students", ["admno" => $admno, "class" => $class, "session" => $session]);
 extract($studentDetails);
 
-$studentPerformance = $query->selectRow(["avg", "pos"], "combr", ["admno"=>$admno, "class"=>$class, "session"=>$session]);
+$studentPerformance = $query->selectRow(["avg", "pos"], "combr", ["admno" => $admno, "class" => $class, "session" => $session]);
 extract($studentPerformance);
 
 $suc = [];
 
 if (isset($_POST['submit']) && isset($_POST['pclass'])) {
-  $date=date('Y-m-d');
+  $date = date('Y-m-d');
 
-   $check = $query->lookUp("admno", "students", ["admno"=>$admno, "session"=>$nsession]);
+  $check = $query->lookUp("admno", "students", ["admno" => $admno, "session" => $nsession]);
 
-   if ($check == 1) {
-     $query->updateQuery("students", ["class" => $_POST['pclass']], ["admno" => $admno, "session" => $nsession]);
-     $query->updateQuery("combr", ["class" => $_POST['pclass']], ["admno" => $admno, "session" => $nsession]);
-      $suc['msg']='STUDENT ALREADY PROMOTED, CLASS UPDATED SUCCESSFULLY';
-   }elseif ($check == 0) {
+  if ($check == 1) {
+    $query->updateQuery("students", ["class" => $_POST['pclass']], ["admno" => $admno, "session" => $nsession]);
+    $query->updateQuery("combr", ["class" => $_POST['pclass']], ["admno" => $admno, "session" => $nsession]);
+    $suc['msg'] = 'STUDENT ALREADY PROMOTED, CLASS UPDATED SUCCESSFULLY';
+  } elseif ($check == 0) {
 
     $query->dbInsert("students", [
-      "name"=>$name, "admno"=>$admno, "dob"=>$dob, "sex"=>$sex, "address"=>$address, "phone"=>$phone,
-      "email"=>$email, "passport"=>$passport, "class"=>$_POST['pclass'], "dater"=>$date, "session"=>$nsession]);
+      "name" => $name,
+      "admno" => $admno,
+      "dob" => $dob,
+      "sex" => $sex,
+      "address" => $address,
+      "phone" => $phone,
+      "email" => $email,
+      "passport" => $passport,
+      "class" => $_POST['pclass'],
+      "dater" => $date,
+      "session" => $nsession
+    ]);
 
-      $checkCombr = $query->lookUp("admno", "combr", ["admno"=>$admno, "session"=>$nsession]);
-      if($checkCombr == 0){
-        $query->dbInsert("combr", ["admno"=>$admno, "class"=>$_POST['pclass'], "session"=>$nsession]);
-      }
-     $suc['msg']='PROMOTED SUCCESSFULLY';
-
-   }
+    $checkCombr = $query->lookUp("admno", "combr", ["admno" => $admno, "session" => $nsession]);
+    if ($checkCombr == 0) {
+      $query->dbInsert("combr", ["admno" => $admno, "class" => $_POST['pclass'], "session" => $nsession]);
+    }
+    $suc['msg'] = 'PROMOTED SUCCESSFULLY';
+  }
 }
-     
-   
-  
+
+
+
 
 require 'view/admin/promote.view.php';
-
